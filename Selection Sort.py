5524de6528350f6f43d37525059c04a231b02689
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-01-24 22:45:51 +0800

    adding def function, loop and assigning different value like minimum

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,4 +1,13 @@
 
-nums = {69, 90, 15, 2, 10, 11, 41, 14, 44, 74}
+nums = {69, 90, 15, 2, 10, 11, 41, 14, 44, 74}     
 
+def sort (nums):
+
+    for i in range (9):
+        minpos = i
+        for j in range (i,10):
+            if nums[j] < nums [minpos]:
+                minpos = j
+
+       
 print(nums)
