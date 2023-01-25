3ee43a474b6dcdb6b158e4aaa23771638d149cfc
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-01-25 12:32:06 +0800

    In each iteration, the function takes the current element and compares it with the elements before it. If it finds an element that is greater than the current element, it shifts that element one position to the right and continues this process until it finds the correct position for the current element

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -3,6 +3,22 @@ nums = [69, 90, 15, 2, 10, 11, 41, 14, 44, 74]
 def insertion_sort(nums):
     n = len(nums)
 
+    for i in range(1,n):
+        key = nums[i]
+# Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+
+        while j >= 0 and key < nums[j]:
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j+1] = key
+
+    return nums
+
+
+
 # Test the function
 
 print("Sorted array is:", insertion_sort(nums))
