363ce209855d316d89ea38581ee63e01e61ea15f
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-01-25 21:15:31 +0800

    the merge process i use i and j index for left and right and also i use while loop for comparison, i also add k value for merged array index

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,4 +1,4 @@
-
+print("THE VALUE:[69, 90, 15, 2, 10, 11, 41, 14, 44, 74]") 
 nums = [69, 90, 15, 2, 10, 11, 41, 14, 44, 74]
 
 def merge_sort(num):
@@ -8,3 +8,28 @@ def merge_sort(num):
 
         merge_sort(left_num)
         merge_sort(right_num)
+
+        i = 0
+        j= 0
+        k= 0 
+        while i < len(left_num) and j < len(right_num):
+            if left_num[i] < right_num[j]:
+                num[k] = left_num[i]
+                i += 1
+            else:
+                num[k] = right_num[j]
+                j+=1
+            k+=1
+
+        while i < len(left_num):
+            num[k] = left_num[i]
+            i+=1
+            k+=1
+
+        while j < len(right_num):
+            num[k] = right_num[j]
+            j+=1
+            k+=1 
+     
+merge_sort(nums)
+print("Sorted array is:",nums)
