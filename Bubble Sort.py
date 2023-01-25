4c02bf4fc08c1122329ddd632827372788aa7f47
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-01-25 11:45:45 +0800

    adding two loop which is for the outer and inner also adding different print valuw to see how it change every loop

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,7 +1,17 @@
-
-
-
+print("THE VALUE:[69, 90, 15, 2, 10, 11, 41, 14, 44, 74]") 
+print("                                                 ")
 nums =  [69, 90, 15, 2, 10, 11, 41, 14, 44, 74]
 
+def bubble_sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                temp =  nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+                print("The process:",nums)
+
 bubble_sort(nums)
-print(nums)
+print("                                                 ")
+print("Sorted array is:",nums)
