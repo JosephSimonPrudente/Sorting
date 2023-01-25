441bd83f0893fdac5a70edce1bc8ccdb97809344
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-01-25 20:57:51 +0800

    defining the function merge_sort and left array, right array

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,28 +1,7 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-    left_half = merge_sort(arr[:mid])
-    right_half = merge_sort(arr[mid:])
-    return merge(left_half, right_half)
-    
-def merge(left, right):
-    print(arr)
-    result = []
-    i = 0
-    j = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    result += left[i:]
-    result += right[j:]
-                
-    return result
 
-arr = [69, 90, 15, 2, 10, 11, 41, 14, 44, 74]
-sorted_arr = merge_sort(arr)
-print(sorted_arr)
+nums = [69, 90, 15, 2, 10, 11, 41, 14, 44, 74]
+
+def merge_sort(num):
+    if len(num)>1:
+        left_num = num [:len(num)//2]
+        right_num = num [len(num)//2:]
